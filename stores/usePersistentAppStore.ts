5370ee4fc6e829c6919c033003f135882f00fa79
farhan10ansari <farhan10ansari@gmail.com>
2025-10-05 21:18:30 +0530

    fix some persistent storage issues

--- stores/usePersistentAppStore.ts
+++ stores/usePersistentAppStore.ts
@@ -1,13 +1,30 @@
-import { CurrencyCode, LocaleValue } from '@/lib/currencies';
+import { CurrencyCode, getCurrencyLocale, LocaleValue } from '@/lib/currencies';
 import { Language } from '@/lib/types';
 import Storage from 'expo-sqlite/kv-store';
 import { create } from 'zustand';
-import { createJSONStorage, persist } from "zustand/middleware";
+import { createJSONStorage, persist, StateStorage } from "zustand/middleware";
 import { getLocales } from 'expo-localization';
+import createDeepMerge from '@fastify/deepmerge'
+const deepMerge = createDeepMerge({ all: true })
 
 const localeDate = getLocales()[0]
-const defaultLocale = localeDate?.languageTag as LocaleValue ?? "en-IN";
 const defaultCurrency = localeDate?.currencyCode as CurrencyCode ?? "INR";
+// const defaultLocale = localeDate?.languageTag as LocaleValue ?? "en-IN";r
+const defaultLocale = getCurrencyLocale(defaultCurrency) as LocaleValue ?? "en-IN";
+
+// Synchronous adapter for Zustand persist (previously saved states were lost randomly)
+const sqliteSyncStorage: StateStorage = {
+    getItem: (name) => {
+        const v = Storage.getItemSync(name);
+        return v ?? null;
+    },
+    setItem: (name, value) => {
+        Storage.setItemSync(name, value);
+    },
+    removeItem: (name) => {
+        Storage.removeItemSync(name);
+    },
+};
 
 type AppSettings = {
     language: Language;
@@ -91,8 +108,15 @@ const usePersistentAppStore = create<PersistentAppStore>()(persist(
         })),
         isDataSeeded: (key) => get().seededKeys.includes(key),
     }), {
-    name: "app-storage",
-    storage: createJSONStorage(() => Storage),
+    name: 'app-storage',
+    storage: createJSONStorage(() => sqliteSyncStorage),
+    merge: (persisted, current) => deepMerge(current, persisted) as never,
+    // Need to implement versioning and migrations in future if needed
+    // version: 1,
+    // migrate: (persistedState, version) => {
+    //     // migration logic here
+    //     return persistedState;
+    // }
 }));
 
 export default usePersistentAppStore;
